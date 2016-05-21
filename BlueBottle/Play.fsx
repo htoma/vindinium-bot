321b4072018894727e4d97dec52e763e13205bb0
Horia Toma <toma.horia@gmail.com>
2016-05-21 15:56:09 +0200

    Priority queue

--- BlueBottle/Play.fsx
+++ BlueBottle/Play.fsx
@@ -2,6 +2,15 @@
 #r "FSharp.Data.2.3.0/lib/net40/FSharp.Data.dll"
 #r @"bin/Debug/BlueBottle.exe"
 
-open Play
-Play.play ""
+//open Play
+//Play.play ""
 
+open Queue
+let queue = Queue<int*int>()
+
+[1;2;7;5]
+|> List.iter (fun v -> queue.push (2,3) v)
+
+while queue.empty() |> not do
+    let (x,y),v=queue.pop()
+    printfn "(%i,%i)=%i" x y v
---
+++ BlueBottle/Queue.fs
@@ -0,0 +1,23 @@
+﻿module Queue
+
+type Queue<'a>() =
+    let mutable list = []  
+    member __.push (key: 'a) (value: int) =
+        let rec push (list: ('a*int) list) =
+            match list with
+            | [] -> [key,value]
+            | (k,v)::tail ->
+                if value>=v then
+                    (key,value)::(k,v)::tail
+                else
+                    (k,v)::(push tail)
+        list<-(push list)
+    member __.empty() =
+        list.Length=0
+    member __.pop() =
+        match list with
+        | [] -> failwith "Empty queue"
+        | head::tail ->
+            list<-tail
+            head    
+
